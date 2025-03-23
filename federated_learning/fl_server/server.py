48df1e7d0d18d24978c58388afa710bc932cb73f
SRshohan <srshohan02@gmail.com>
2025-03-22 23:44:31 -0400

    Added server for federated learning

---
+++ federated_learning/fl_server/server.py
@@ -0,0 +1,13 @@
+import flwr as fl
+
+
+strategy = fl.server.strategy.FedAvg(
+    min_available_clients=2,
+)
+
+
+fl.server.start_server(
+    server_address="192.168.1.14:8080",
+    config=fl.server.ServerConfig(num_rounds=3),
+    strategy=strategy,
+)
